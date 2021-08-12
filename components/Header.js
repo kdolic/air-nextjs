b71737f2dc505750a54e42cfea0a6a2483af67f9
Kenan Dolic <kenandolic@Kenans-MacBook-Pro.local>
2021-08-12 13:31:47 -0500

    config file and left-side of Header

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,17 @@
+import Image from 'next/image'
+
 function Header() {
     return (
-        <header>
+        <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
             {/* Left */}
-            <div></div>
+            <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+                <Image 
+                    src='https://links.papareact.com/qd3'
+                    layout='fill'
+                    objectFit='contain'
+                    objectPosition='left'
+                />
+            </div>
             {/* Middle */}
             <div></div>
             {/* Right */}
