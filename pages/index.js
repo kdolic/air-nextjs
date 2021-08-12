6b0f0f495a3567e4aa56665874583eea6f96d94a
Kenan Dolic <kenandolic@Kenans-MacBook-Pro.local>
2021-08-12 15:40:56 -0500

    setup main content & getStaticProps function

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,28 @@ export default function Home() {
       <Header/>
       <Banner/>
 
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+
+          {/* Pull data from a server - API endpoints */}
+          
+        </section>
+      </main>
+
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const exploreData = await (await fetch('https://links.papareact.com/pyp')
+    .then((res) => res.json)
+    .catch(err => console.log(`Error: ${err}`))
+  );
+
+  return {
+    props: {
+      exploreData
+    }
+  }
 }
