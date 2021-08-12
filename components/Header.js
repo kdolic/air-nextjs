16906292251477a4cd7360fd67241ec6ed84df90
Kenan Dolic <kenandolic@Kenans-MacBook-Pro.local>
2021-08-12 13:07:41 -0500

    index cleanup and Header component setup

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <header>
+            {/* Left */}
+            <div></div>
+            {/* Middle */}
+            <div></div>
+            {/* Right */}
+            <div></div>
+        </header>
+    )
+}
+
+export default Header
