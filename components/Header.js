0034d302a636d18f42ecacd6bf746c6e88e5f6be
Kenan Dolic <kenandolic@Kenans-MacBook-Pro.local>
2021-08-13 14:29:24 -0500

    implemented useState for the Header searchbar

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,10 @@
 import Image from 'next/image'
 import { GlobeAltIcon, MenuIcon, UserCircleIcon, UsersIcon, SearchIcon } from '@heroicons/react/solid'
+import { useState } from 'react';
 
 function Header() {
+    const [searchInput, setSearchInput] = useState('');
+
     return (
         <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
             {/* Left - Logo */}
@@ -15,7 +18,7 @@ function Header() {
             </div>
             {/* Middle - Search */}
             <div className='flex items-center border-2 rounded-full py-2 md:shadow-sm'>
-                <input className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400' type='text' placeholder='Start your search' />
+                <input className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400' value={searchInput} onChange={(e) => setSearchInput(e.target.value)} type='text' placeholder='Start your search' />
                 <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
             </div>
             {/* Right - Menu */}
