7d5991c836ba5cfb5044cd6126e8046fad38c6eb
Kenan Dolic <kenandolic@Kenans-MacBook-Pro.local>
2021-08-13 17:11:29 -0500

    developed search functionality and page, started InfoCard

--- components/Header.js
+++ components/Header.js
@@ -4,12 +4,14 @@ import { useState } from 'react';
 import 'react-date-range/dist/styles.css' // main style file
 import 'react-date-range/dist/theme/default.css' // theme css file
 import { DateRangePicker } from 'react-date-range'
+import { useRouter } from 'next/dist/client/router';
 
-function Header() {
+function Header({ placeholder }) {
     const [searchInput, setSearchInput] = useState('');
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
     const [guestNum, setGuestNum] = useState(1);
+    const router = useRouter();
 
     const handleSelect = (ranges) => {
         setStartDate(ranges.selection.startDate);
@@ -18,6 +20,18 @@ function Header() {
 
     const resetInput = () => {
         setSearchInput('');
+    }
+
+    const search = () => {
+        router.push({
+            pathname: '/search',
+            query: {
+                location: searchInput,
+                startDate: startDate.toISOString(),
+                endDate: endDate.toISOString(),
+                guestNum,
+            }
+        });
     }
 
     const selectionRange = {
@@ -29,7 +43,7 @@ function Header() {
     return (
         <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
             {/* Left - Logo */}
-            <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+            <div onClick={() => router.push('/')} className='relative flex items-center h-10 cursor-pointer my-auto'>
                 <Image 
                     src='https://links.papareact.com/qd3'
                     layout='fill'
@@ -39,7 +53,7 @@ function Header() {
             </div>
             {/* Middle - Search */}
             <div className='flex items-center border-2 rounded-full py-2 md:shadow-sm'>
-                <input className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400' value={searchInput} onChange={(e) => setSearchInput(e.target.value)} type='text' placeholder='Start your search' />
+                <input className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400' value={searchInput} onChange={(e) => setSearchInput(e.target.value)} type='text' placeholder={placeholder || 'Start your search'} />
                 <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
             </div>
             {/* Right - Menu */}
@@ -68,7 +82,7 @@ function Header() {
                     </div>
                     <div className='flex'>
                         <button onClick={resetInput} className='flex-grow text-gray-500'>Cancel</button>
-                        <button className='flex-grow text-red-400'>Search</button>
+                        <button onClick={search} className='flex-grow text-red-400'>Search</button>
                     </div>
                 </div>
             )}
